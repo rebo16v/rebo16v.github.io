e073474656f9688a4dd980f199d2b7c8142ca0de
erebollo@capgemini.com <erebollo@capgemini.com>
2023-02-13 17:24:04 +0100

    prophecy

--- montecarlo.js
+++ montecarlo.js
@@ -1,22 +1,29 @@
 async function montecarlo() {
   await Excel.run(async(context) => {
     const n_iter = parseInt(document.getElementById("niter").value);
-    let app = context.workbook.application;
-    app.suspendApiCalculationUntilNextSync();
-    ranges.forEach(r => {
-      let [s, c] = r.split("!");
-      let s2 = context.workbook.worksheets.getItem(s);
-      let c2 = s2.getRange(c);
-      c2.values = [["input"]]
-    });
-    return context.sync().then(function() {
-      forecasts.forEach(f => {
-        let [s, c] = f.split("!");
+    var prophecy = context.workbook.worksheets.add("prophecy")
+    range = prophecy.getRange("A" + 2 + ":E" + (2+randoms.length));
+    range.load("values");
+    context.sync().then(function() {
+      let conf = range.values;
+      console.log("conf => " + conf)
+      let app = context.workbook.application;
+      app.suspendApiCalculationUntilNextSync();
+      randoms.forEach((r,i) => {
+        let [s, c] = r.split("!");
         let s2 = context.workbook.worksheets.getItem(s);
         let c2 = s2.getRange(c);
-        c2.load("values");
-        context.sync().then(function() {
-          console.log("f => " + c2.values);
+        c2.values = [["input"]]
+      });
+      return context.sync().then(function() {
+        forecasts.forEach(f => {
+          let [s, c] = f.split("!");
+          let s2 = context.workbook.worksheets.getItem(s);
+          let c2 = s2.getRange(c);
+          c2.load("values");
+          context.sync().then(function() {
+            console.log("f => " + c2.values);
+          });
         });
       });
     });
