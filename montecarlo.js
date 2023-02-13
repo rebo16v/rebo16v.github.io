8f8d7124af2167ddb9710f9b483f13fc835dce87
erebollo@capgemini.com <erebollo@capgemini.com>
2023-02-13 18:35:28 +0100

    prophecy

--- montecarlo.js
+++ montecarlo.js
@@ -1,21 +1,28 @@
 async function montecarlo() {
   await Excel.run(async(context) => {
     const n_iter = parseInt(document.getElementById("niter").value);
+    let app = context.workbook.application;
     var prophecy = context.workbook.worksheets.getItem("prophecy");
     range = prophecy.getRange("A" + 2 + ":G" + (2+randoms.length));
     range.load("values");
     context.sync().then(function() {
-      let conf = range.values;
-      conf.forEach(c => {console.log("=> "+ c)});
-      console.log("conf => " + conf)
-      let app = context.workbook.application;
-      app.suspendApiCalculationUntilNextSync();
-      randoms.forEach((r,i) => {
-        let [s, c] = r.split("!");
-        let s2 = context.workbook.worksheets.getItem(s);
-        let c2 = s2.getRange(c);
-        c2.values = [["input"]]
-      });
+      let confs = range.values;
+      for (let i = 0; i < n_iter; i++) {
+        app.suspendApiCalculationUntilNextSync();
+        console.log("iter => " + i);
+        stepIn(confs);
+        context.sync().then(function() {
+          let o = stepOut();
+          console.log("output => " + o);
+        });
+      }
+    });
+  });
+}
+
+
+
+
       return context.sync().then(function() {
         forecasts.forEach(f => {
           let [s, c] = f.split("!");
@@ -27,6 +34,39 @@ async function montecarlo() {
           });
         });
       });
+    });
+  });
+}
+
+async function stepIn(confs) {
+  confs.forEach(conf => {
+    console.log("conf => "+ conf)
+    let input = 0;
+    switch (conf[3]) {
+      case "uniform":
+        input = sampleUniform(conf[4], conf[5]);
+      case "normal":
+        input = sampleUniform(conf[4], conf[5]);
+      case "triangular":
+        input = sampleUniform(conf[4], conf[5], conf[6]);
+    }
+    let [s, c] = conf[1].split("!");
+    let sheet = context.workbook.worksheets.getItem(s);
+    let cell = sheet.getRange(c);
+    cell.values = [[input]];    
+  });
+}
+
+async function stepOut() {
+  forecasts.forEach(f => {
+    console.log("f => "+ f)
+    let [s, c] = f.split("!");
+    let sheet = context.workbook.worksheets.getItem(s);
+    let cell = sheet.getRange(c);
+    cell.load("values");
+    context.sync().then(function() {
+      let output = cell.values
+      console.log("output => " + output);
     });
   });
 }
