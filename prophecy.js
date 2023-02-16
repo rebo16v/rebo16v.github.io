4adbc9cc2bc5f1716cd9a1a420325807807780de
erebollo@capgemini.com <erebollo@capgemini.com>
2023-02-16 14:47:34 +0100

    prophecy

--- prophecy.js
+++ prophecy.js
@@ -1,5 +1,5 @@
-var randoms = [];
-var forecasts = [];
+let randoms = [];
+let forecasts = [];
 
 Office.onReady((info) => {
     if (info.host === Office.HostType.Excel) {
@@ -17,7 +17,7 @@ Office.onReady((info) => {
                 console.log("prophecy found!")
               }
               else {
-                var prophecy = context.workbook.worksheets.add("prophecy")
+                let prophecy = context.workbook.worksheets.add("prophecy")
                 range1 = prophecy.getRange("A1:E1");
                 range1.values = [["name", "cell", "value", "distribution", "parameters"]];
                 range1.format.fill.color = "yellow";
@@ -32,10 +32,10 @@ Office.onReady((info) => {
 
 async function workbookChange(event) {
     await Excel.run(async (context) => {
-      var cell = context.workbook.getActiveCell();
+      let cell = context.workbook.getActiveCell();
       cell.load("address");
       return context.sync().then(function() {
-        var address = cell.address
+        let address = cell.address
         if (randoms.includes(address)) {
           document.getElementById('input').checked = true;
           document.getElementById('distro').disabled = false;
@@ -54,21 +54,21 @@ async function radioChange(event) {
   await Excel.run(async (context) => {
     console.log("randoms => " +  randoms);
     console.log("forecasts => + forecasts");
-    var sheet = context.workbook.worksheets.getActiveWorksheet();
-    var cell = context.workbook.getActiveCell();
-    var prophecy = context.workbook.worksheets.getItem("prophecy")
+    let sheet = context.workbook.worksheets.getActiveWorksheet();
+    let cell = context.workbook.getActiveCell();
+    let prophecy = context.workbook.worksheets.getItem("prophecy")
     cell.load("address");
     cell.load("values")
     cell.load("numberFormat")
     return context.sync().then(function() {
-      var address = cell.address
-      var idx = randoms.indexOf(address);
-      var idx2 = forecasts.indexOf(address);
+      let address = cell.address
+      let idx = randoms.indexOf(address);
+      let idx2 = forecasts.indexOf(address);
       if (document.getElementById('input').checked) {
           document.getElementById('distro').disabled = false;
           if (idx == -1) {
             randoms.push(address)
-            var row = randoms.length
+            let row = randoms.length
             prophecy.getCell(row, 0).values = [["input_" + row]]
             prophecy.getCell(row, 1).hyperlink = {
                 textToDisplay: address,
@@ -89,7 +89,7 @@ async function radioChange(event) {
           }
           if (idx2 != -1) {
             forecasts.splice(idx2, 1);
-            var range = prophecy.getRange("I" + (2+idx2) + ":K" + (2+idx2));
+            let range = prophecy.getRange("I" + (2+idx2) + ":K" + (2+idx2));
             range.delete(Excel.DeleteShiftDirection.up);
           }
           cell.format.fill.color = "yellow"
@@ -97,12 +97,12 @@ async function radioChange(event) {
           document.getElementById('distro').disabled = true;
           if (idx != -1) {
             randoms.splice(idx, 1);
-            var range = prophecy.getRange("A" + (2+idx) + ":Z" + (2+idx));
+            let range = prophecy.getRange("A" + (2+idx) + ":Z" + (2+idx));
             range.delete(Excel.DeleteShiftDirection.up);
           }
           if (idx2 == -1) {
             forecasts.push(address);
-            var row = forecasts.length
+            let row = forecasts.length
             prophecy.getCell(row, 8).values = [["output_" + row]]
             prophecy.getCell(row, 9).hyperlink = {
                 textToDisplay: address,
@@ -116,12 +116,12 @@ async function radioChange(event) {
           document.getElementById('distro').disabled = true;
           if (idx != -1) {
             randoms.splice(idx, 1);
-            var range = prophecy.getRange("A" + (2+idx) + ":Z" + (2+idx));
+            let range = prophecy.getRange("A" + (2+idx) + ":Z" + (2+idx));
             range.delete(Excel.DeleteShiftDirection.up);
           }
           if (idx2 != -1) {
             forecasts.splice(idx2, 1);
-            var range = prophecy.getRange("I" + (2+idx2) + ":K" + (2+idx2));
+            let range = prophecy.getRange("I" + (2+idx2) + ":K" + (2+idx2));
             range.delete(Excel.DeleteShiftDirection.up);
           }
           cell.format.fill.clear();
@@ -132,13 +132,13 @@ async function radioChange(event) {
 
 async function distro(event) {
   await Excel.run(async (context) => {
-    var cell = context.workbook.getActiveCell();
+    let cell = context.workbook.getActiveCell();
     cell.load("address");
     return context.sync().then(function() {
       row = 2 + randoms.indexOf(cell.address);
-      var prophecy = context.workbook.worksheets.getItem("prophecy");
+      let prophecy = context.workbook.worksheets.getItem("prophecy");
       prophecy.activate();
-      var range = prophecy.getRange("A" + row + ":Z" + row);
+      let range = prophecy.getRange("A" + row + ":Z" + row);
       range.select()
     });
   });
